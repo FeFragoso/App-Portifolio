3955c4b5e265aa17d628e9052936b559845a8110
fefragoso <felipeofragoso@gmail.com>
2022-08-14 23:33:45 -0300

    Criação do titulo e cover

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const cover = require('./assets/cover.png')
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={s.container}>
+      <ImageBackground
+        source={cover}
+        resizeMode='cover'
+        style={s.fundo}
+      >
+
+        <Text style={s.h1}>Hello<Text style={{ color: '#D6382F' }}>.</Text>{'\n'}I am Felipe</Text>
+
+      </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0f0',
+    fontFamily: 'sans-serif'
   },
+  fundo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  h1: {
+    color: '#fff',
+    fontSize: 45,
+    fontWeight: 'bold',
+    textShadowColor: '3.5px 3.5px #174e6d'
+  }
 });
