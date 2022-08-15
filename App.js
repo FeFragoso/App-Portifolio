40b0b6da51f1322b327a539922780a7d5a30cc54
fefragoso <felipeofragoso@gmail.com>
2022-08-15 16:37:07 -0300

    Adição de estilização

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-
-const cover = require('./assets/cover.png')
+import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={s.container}>
       <ImageBackground
-        source={cover}
+        source={require('./assets/cover.png')}
         resizeMode='cover'
         style={s.fundo}
       >
 
-        <Text style={s.h1}>Hello<Text style={{ color: '#D6382F' }}>.</Text>{'\n'}I am Felipe</Text>
+        <View style={s.header}>
+          <Image
+            source={require('./assets/foto.jpg')}
+            style={s.foto}
+          />
+
+          <Text style={s.h1}>Hello<Text style={{ color: '#D6382F' }}>.</Text>{'\n'}I am Felipe</Text>
+        </View>
 
       </ImageBackground>
     </View>
@@ -21,14 +25,30 @@ export default function App() {
 
 const s = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#0f0',
-    fontFamily: 'sans-serif'
+    flex: 1
   },
   fundo: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center'
+  },
+  header: {
+    flex: 0.7,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
+    width: '100%',
+
+    borderWidth: 5,
+    borderColor: '#fff'
+  },
+  foto: {
+    width: 300,
+    height: 300,
+
+    borderRadius: 150,
+    borderWidth: 5,
+    borderColor: '#fff'
   },
   h1: {
     color: '#fff',
